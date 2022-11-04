6b567158202cabc9337467979a66589d65d9a2a5
m4ul <hansonlucas06@gmail.com>
2022-11-04 14:04:10 +0100

    first commit

---
+++ mitm.py
@@ -0,0 +1,17 @@
+from scapy.all import ARP, Ether, srp
+
+
+result = srp((Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="192.168.1.0/24")), timeout=3, verbose=0)[0]
+
+print("Online IPs:")
+ip=[]
+mac=[]
+for sent, received in result:
+    ip.append(received.psrc)
+    mac.append(received.hwsrc)
+
+for i in range (len(ip)):
+    print(ip[i], " = ", mac[i])
+
+    
+    
