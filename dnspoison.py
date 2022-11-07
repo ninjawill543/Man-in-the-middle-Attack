a462fcbf46e5762115a03c8d1f090b8e87c8a999
m4ul <hansonlucas06@gmail.com>
2022-11-07 15:02:06 +0100

    first dns

--- dnspoison.py
+++ dnspoison.py
@@ -1,7 +1,7 @@
 from scapy.all import DNS, DNSQR, IP, send, IPv6, sr, UDP, sniff, DNSRR, sendp, Ether, srp1, ARP
 
 
-pkts = sniff(count=10,filter="udp dst port 53")
+pkts = sniff(count=10,filter="udp dst port 53 host 10.5.1.2 ")
 
 pkts.nsummary()
 
