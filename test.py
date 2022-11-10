d9b4ca9984b837bf97c3d2287231a4e2815efbdd
m4ul <hansonlucas06@gmail.com>
2022-11-10 14:40:59 +0100

    test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 
 
-a=sniff(count=10, prn=lambda x: x.show(), filter="port 53 qr 1",promisc=1)
+a=sniff(count=10, prn=lambda x: x.show(), filter="port 53 and qr 1",promisc=1)
