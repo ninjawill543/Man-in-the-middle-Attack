84afacf70c9083b3c31d3990a538478dd8c19655
m4ul <hansonlucas06@gmail.com>
2022-11-10 14:39:46 +0100

    test

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 
 
-a=sniff(count=10, prn=lambda x: x.show(), filter="port 53",promisc=1)
-if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==1:
-    print (a[0].getlayer(DNS).qd.qname)
-    print (a[0].getlayer(DNS).qd.qtype)
-    print (a[0].getlayer(DNS).qd.qclass)
-    print (a[0].getlayer(DNS).id)
-    print (a[0].getlayer(DNS).an)
-    print (a[0].getlayer(DNS).ns)
-    print (a[0].getlayer(DNS).ar)
+a=sniff(count=10, prn=lambda x: x.show(), filter="port 53 qr 1",promisc=1)
