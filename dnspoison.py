214fb0bbd2727aec065ed10329ca7d7bc71b517e
m4ul <hansonlucas06@gmail.com>
2022-11-10 19:23:16 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -4,5 +4,10 @@ from scapy.all import sniff, DNS, DNSQR, DNSRR
 a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
     print (a[0].getlayer(DNS).qd.qname)
-    
+    print (a[0].getlayer(DNS).qd.qtype)
+    print (a[0].getlayer(DNS).qd.qclass)
+    print (a[0].getlayer(DNS).id)
+    print (a[0].getlayer(DNS).an)
+    print (a[0].getlayer(DNS).ns)
+    print (a[0].getlayer(DNS).ar)
 
--- test.py
+++ test.py
@@ -2,6 +2,11 @@ from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 
 
 a=sniff(count=5, prn=lambda x: x.show(), filter="port 53",promisc=1)
+if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
+    print (a[0].getlayer(DNS).qd.qname)
+    f = open("test.txt", "a")
+    f.write(a[0].getlayer(DNS).qd.qname)
+    f.close()
     #ipdest = 
     #udpdestport = 
     #dnsid = 
