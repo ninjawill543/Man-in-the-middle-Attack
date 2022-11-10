db59b875e404ee8c8d90d2204b2c7fb54c15b852
m4ul <hansonlucas06@gmail.com>
2022-11-10 15:15:02 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -3,13 +3,7 @@ from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 
 a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    srp((IP(dst=routermac)/ARP(op = 2,pdst=routerip, psrc=ip[victim])), timeout=3, verbose=0)
-    print (a[0].getlayer(DNS).qd.qname)
-    print (a[0].getlayer(DNS).qd.qtype)
-    print (a[0].getlayer(DNS).qd.qclass)
-    print (a[0].getlayer(DNS).id)
-    print (a[0].getlayer(DNS).an)
-    print (a[0].getlayer(DNS).ns)
-    print (a[0].getlayer(DNS).ar)
+    print (a[0].getlayer(DNS))
+   
     
 
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 
 
-a=sniff(count=10, prn=lambda x: x.show(), filter="port 53",promisc=1)
-if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==1:
-    print (a[0].getlayer(DNS))
+a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
+#if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
+    #ipdest = 
+    #udpdestport = 
+    #dnsid = 
+
