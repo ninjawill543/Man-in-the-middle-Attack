b43fe779ffc6796aae05e445e24fbc147e7ceb14
m4ul <hansonlucas06@gmail.com>
2022-11-10 15:25:40 +0100

    test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0 and a[0].getlayer(DNS).qd.qname == "b'reverseproxy.ynov.com'":
     print("it worked")
 else:
-    print("nono")
+    print(a[0].getlayer(DNS).qd.qname)
     #ipdest = 
     #udpdestport = 
     #dnsid = 
