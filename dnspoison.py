8af1e38f9fcdd8cf8e1496e2c187a30dbaf87ff8
m4ul <hansonlucas06@gmail.com>
2022-11-10 15:17:52 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -1,9 +1,8 @@
-from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
+from scapy.all import sniff, DNS, DNSQR, DNSRR
 
 
 a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    print (a[0].getlayer(DNS).qname)
-   
+    print (a[0].getlayer(DNS).qd.qname)
     
 
