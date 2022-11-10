45b476d939d0188f0fe343a2d8b255224709a381
m4ul <hansonlucas06@gmail.com>
2022-11-10 19:29:06 +0100

    test

--- test.py
+++ test.py
@@ -2,11 +2,8 @@ from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP
 
 
 a=sniff(count=5, prn=lambda x: x.show(), filter="port 53",promisc=1)
-if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    print (a[0].getlayer(DNS).qd.qname)
-    f = open("test.txt", "a")
-    f.write(str(a[0].getlayer(DNS).qd.qname))
-    f.close()
+if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0 and a[0].getlayer(DNS).qd.qname=="b'reverseproxy.ynov.com.'":
+    print("yes")
     #ipdest = 
     #udpdestport = 
     #dnsid = 
