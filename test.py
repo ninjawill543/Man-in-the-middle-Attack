937282e7040cc8e8c3f8d6a98f1d864e7d1dabfd
m4ul <hansonlucas06@gmail.com>
2022-11-10 19:42:49 +0100

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ domain = 'reverseproxy.ynov.com.'
 
 a=sniff(count=1, prn=lambda x: x.show(), filter="port 53",promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    if domain in a[DNS].qd.qname:
+    if domain in a[0].getlayer(DNS).qd.qname:
         print("it works")
     #ipdest = 
     #udpdestport = 
