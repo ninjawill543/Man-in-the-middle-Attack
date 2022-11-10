2663c8e72b5a39fa8332fc2f05a6e388bc81f55e
m4ul <hansonlucas06@gmail.com>
2022-11-10 19:24:08 +0100

    test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ a=sniff(count=5, prn=lambda x: x.show(), filter="port 53",promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
     print (a[0].getlayer(DNS).qd.qname)
     f = open("test.txt", "a")
-    f.write(a[0].getlayer(DNS).qd.qname)
+    f.write(str(a[0].getlayer(DNS).qd.qname))
     f.close()
     #ipdest = 
     #udpdestport = 
