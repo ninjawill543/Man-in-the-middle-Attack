aaa4d44458462d239698c02a48569679b38f858e
m4ul <hansonlucas06@gmail.com>
2022-11-10 14:28:11 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -2,12 +2,12 @@ from scapy.all import sniff, DNS, DNSQR, DNSRR
 
 
 if (sniff(prn=lambda x: x.show(), filter="port 53",promisc=1))[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    print (a[0].getlayer(DNS).qd.qname)
-    print (a[0].getlayer(DNS).qd.qtype)
-    print (a[0].getlayer(DNS).qd.qclass)
-    print (a[0].getlayer(DNS).id)
-    print (a[0].getlayer(DNS).an)
-    print (a[0].getlayer(DNS).ns)
-    print (a[0].getlayer(DNS).ar)
+    print ([0].getlayer(DNS).qd.qname)
+    print ([0].getlayer(DNS).qd.qtype)
+    print ([0].getlayer(DNS).qd.qclass)
+    print ([0].getlayer(DNS).id)
+    print ([0].getlayer(DNS).an)
+    print ([0].getlayer(DNS).ns)
+    print ([0].getlayer(DNS).ar)
     
 
